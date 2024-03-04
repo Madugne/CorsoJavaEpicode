71b9356dfe52b67769ddb172741c269fd083a6b4
Fabio Yu <fabioyu7665@gmail.com>
2024-03-04 14:44:04 +0100

    Esercizio1

--- Test/src/Esercizio1.java
+++ Test/src/Esercizio1.java
@@ -4,4 +4,9 @@ public class Esercizio1 {
         risultato = numero1*numero2;
         System.out.println("il risultato e': " + risultato);
     }
+    public static void concatena (String stringa, int numero) {
+        String catena;
+        catena = stringa + numero;
+        System.out.println(catena);
+    }
 }
--- Test/src/Main.java
+++ Test/src/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
         Esercizio1.moltiplica(2, 2);
+        Esercizio1.concatena("numero", 5);
     }
 }
