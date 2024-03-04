8c77ddfa57b3701a59334c917bd64585acef4ab9
Fabio Yu <fabioyu7665@gmail.com>
2024-03-04 14:34:42 +0100

    Esercizio1

---
+++ Test/src/Esercizio1.java
@@ -0,0 +1,7 @@
+public class Esercizio1 {
+    public static void moltiplica (int numero1, int numero2) {
+        int risultato;
+        risultato = numero1*numero2;
+        System.out.println("il risultato e': " + risultato);
+    }
+}
