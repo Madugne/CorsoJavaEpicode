4ee81f22f337ae51b883ffb700937cef8e7f973a
Fabio Yu <fabioyu7665@gmail.com>
2024-03-11 15:58:44 +0100

    Esercizio 2 svolto

--- Esercizio11-03-24/src/Esercizio2/Main.java
+++ Esercizio11-03-24/src/Esercizio2/Main.java
@@ -6,16 +6,26 @@ public class Main {
     Scanner scanner = new Scanner(System.in);
     try {
         System.out.println("Inserisci km percorsi");
-        int km = scanner.nextInt();
+        int km = Integer.parseInt(scanner.nextLine());
 
         System.out.println("Inserisci litri di carburante usati");
-        int litri = scanner.nextInt();
+        int litri = Integer.parseInt(scanner.nextLine());
 
-        if (km < 0) 
+        if (km < 0) {
+            throw new IllegalArgumentException("Errore: non puoi aver percorso km negativi");
+        }
 
         if (litri == 0 || litri < 0) {
             throw new ArithmeticException("Errore: non si puo' dividere per zero o meno");
         }
+        double kmL = (double) km / litri;
+        System.out.println("Il consumo e': " + kmL + "km/l");
+    } catch (NumberFormatException errore) {
+        System.out.println("Input non valido. Inserire un numero.");
+    } catch (ArithmeticException | IllegalArgumentException errore) {
+        System.out.println("Errore: " + errore.getMessage());
+    } finally {
+        scanner.close();
     }
     }
 }
