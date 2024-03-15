ae5c915a177f33ee272d1b3ba42e9a965b19af15
Fabio Yu <fabioyu7665@gmail.com>
2024-03-15 11:33:43 +0100

    Update Application.java

--- ProgettoSettimanaleJava2/src/main/java/chunyinyu/Application.java
+++ ProgettoSettimanaleJava2/src/main/java/chunyinyu/Application.java
@@ -3,6 +3,6 @@ package chunyinyu;
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World");
     }
 }
